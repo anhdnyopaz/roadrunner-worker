b13e26acbb082a2cf7a65c9b7e1698cf9c774c74
Ngoc Anh Duong <anhdn@yopaz.vn>
2024-06-07 23:16:36 +0700

    Update Docker file

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
-FROM php:8.1-alpine
+FROM infoecnet/php8.1-laravel-alpine:v1
 COPY . /app
 WORKDIR /app
-RUN composer install
+EXPOSE 8080
 CMD ["rr", "serve"]
--- index.php
+++ index.php
@@ -13,30 +13,30 @@ $worker = Worker::create();
 $factory = new Psr17Factory();
 
 $psr7 = new PSR7Worker($worker,$factory,$factory,$factory);
-
-$server = new Server(new Invoker(),[
-    'debug' => true,
-]);
-
-$server->registerService(\Example\ExampleServiceInterface::class, new \Services\ExampleService());
-
-$server->serve(Worker::create());
 //
-//while (true) {
-//    try {
-//        $request = $psr7->waitRequest();
-//        if($request == null){
-//            break;
-//        }
-//    } catch (\Throwable $e) {
-//        $psr7->respond(new Response(400));
-//        continue;
-//    }
+//$server = new Server(new Invoker(),[
+//    'debug' => true,
+//]);
 //
-//    try {
-//        $psr7->respond(new Response(200, [], 'Hello World!'));
-//    } catch (\Throwable $e) {
-//        $psr7->respond(new Response(500, [], 'Internal Server Error'));
-//        $psr7->getWorker()->error((string)$e);
-//    }
-//}
+//$server->registerService(\Example\ExampleServiceInterface::class, new \Services\ExampleService());
+//
+//$server->serve(Worker::create());
+
+while (true) {
+    try {
+        $request = $psr7->waitRequest();
+        if($request == null){
+            break;
+        }
+    } catch (\Throwable $e) {
+        $psr7->respond(new Response(400));
+        continue;
+    }
+
+    try {
+        $psr7->respond(new Response(200, [], 'Hello World!'));
+    } catch (\Throwable $e) {
+        $psr7->respond(new Response(500, [], 'Internal Server Error'));
+        $psr7->getWorker()->error((string)$e);
+    }
+}
